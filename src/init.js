4657430a7f87949b20bc6ecb417df4b1468b96fe
Sharanya012 <sharanyasundar.g@gmail.com>
2024-11-19 06:07:16 +0530

    UI Added, Changed Sky colour

--- src/init.js
+++ src/init.js
@@ -49,13 +49,13 @@ export async function init(setupScene = () => {}, onFrame = () => {}) {
 	document.body.appendChild(container);
 
 	const scene = new THREE.Scene();
-	scene.background = new THREE.Color(0x808080);
+	scene.background = new THREE.Color(0xF5FBFF);
 
 	const camera = new THREE.PerspectiveCamera(
 		100,
 		window.innerWidth / window.innerHeight,
 		0.1,
-		50,
+		150,
 	);
 	camera.position.set(-27, 1.5, 50);
 
